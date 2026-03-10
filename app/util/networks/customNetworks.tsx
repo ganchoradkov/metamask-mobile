5f7a0133cedb5cf608cbeac99d501228016ca065
jeremy-consensys <jeremy.tsang-ext@consensys.net>
2026-03-10 17:09:59 +0800

    chore: add stable network

--- app/util/networks/customNetworks.tsx
+++ app/util/networks/customNetworks.tsx
@@ -388,6 +388,7 @@ export const NETWORK_CHAIN_ID: {
   readonly ROOTSTOCK_TESTNET: '0x1f';
   readonly TEMPO_TESTNET: '0xa5bf';
   readonly CHILIZ: '0x15b38';
+  readonly STABLE_MAINNET: '0x3dc';
 } & typeof CHAIN_IDS = {
   FLARE_MAINNET: '0xe',
   SONGBIRD_TESTNET: '0x13',
@@ -432,6 +433,7 @@ export const NETWORK_CHAIN_ID: {
   ROOTSTOCK_TESTNET: '0x1f',
   TEMPO_TESTNET: '0xa5bf',
   CHILIZ: '0x15b38',
+  STABLE_MAINNET: '0x3dc',
   ...CHAIN_IDS,
 };
 
@@ -481,4 +483,5 @@ export const CustomNetworkImgMapping: Record<Hex, string> = {
   [NETWORK_CHAIN_ID.ROOTSTOCK_TESTNET]: require('../../images/rootstock.png'),
   [NETWORK_CHAIN_ID.TEMPO_TESTNET]: require('../../images/tempo.png'),
   [NETWORK_CHAIN_ID.CHILIZ]: require('../../images/chiliz.png'),
+  [NETWORK_CHAIN_ID.STABLE_MAINNET]: require('../../images/stable.png'),
 };
